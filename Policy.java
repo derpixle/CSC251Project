1435d4951f247a5a9fbf4241482603a474c35983
Matthew Viegelmann <mviegel0@email.cpcc.edu>
2024-02-19 17:56:08 -0500

    Reuploading Project 2 (forgot to save local files?)

--- Policy.java
+++ Policy.java
@@ -16,6 +16,9 @@ public class Policy
    
    
    //constructors
+   /**
+   *  This constructor is used to create a blank Policy object
+   */
    public Policy()
    {
       policyNum = 0;
@@ -28,6 +31,17 @@ public class Policy
       userWeight = 0;
    }
    
+   /**
+   *  This constructor is used to create a Policy object
+   *  @param polNum     This is the policy number
+   *  @param proName    This is the provider name
+   *  @param useFirst   This is the user's first name
+   *  @param useLast    This is the user's last name
+   *  @param aNum       This is the user's age
+   *  @param sStat      This is the user's smoking status
+   *  @param height     This is the user's height in inches
+   *  @param weight     This is the user's weight in pounds
+   */
    public Policy(int polNum, String proName, String useFirst, String useLast,int aNum, String sStat, double height, double weight)
    {
       policyNum = polNum;
@@ -45,41 +59,73 @@ public class Policy
    
    
    //setters
+   /**
+   *  This setter is used to set the Policy Number
+   *  @param polNum     This is the policy number
+   */
    public void setPolicyNumber(int polNum)
    {
       policyNum = polNum;
    }
    
+   /**
+   *  This setter is used to set the Provider Name
+   *  @param proName    This is the provider name
+   */
    public void setProviderName(String proName)
    {
       providerName = proName;
    }
    
+   /**
+   *  This setter is used to set the User's First Name
+   *  @param useFirst   This is the user's first name
+   */
    public void setFirstName(String useFirst)
    {
       userFirst = useFirst;
    }
    
+   /**
+   *  This setter is used to set the User's Last Name
+   *  @param useLast    This is the user's last name
+   */
    public void setLastName(String useLast)
    {
       userLast = useLast;
    }
    
+   /**
+   *  This setter is used to set the User's Age
+   *  @param aNum       This is the user's age
+   */
    public void setAge(int aNum)
    {
       userAge = aNum;
    }
    
+   /**
+   *  This setter is used to set the User's Smoking Status
+   *  @param sStat      This is the user's smoking status
+   */
    public void setSmokingStatus(String sStat)
    {
       smokingStatus = sStat;
    }
    
+   /**
+   *  This setter is used to set the User's Height
+   *  @param height     This is the user's height in inches
+   */
    public void setHeight(double height)
    {
       userHeight = height;
    }
    
+   /**
+   *  This setter is used to set the User's Weight
+   *  @param weight     This is the user's weight in pounds
+   */
    public void setWeight(double weight)
    {
       userWeight = weight;
@@ -91,41 +137,65 @@ public class Policy
    
    
    //getters
+   /**
+   *  This getter is used to get the Policy Number
+   */
    public int getPolicyNumber()
    {
       return policyNum;
    }
    
+   /**
+   *  This getter is used to get the Provider Name
+   */
    public String getProviderName()
    {
       return providerName;
    }
    
+   /**
+   *  This getter is used to get the User's First Name
+   */
    public String getFirstName()
    {
       return userFirst;
    }
    
+   /**
+   *  This getter is used to get the User's Last Name
+   */
    public String getLastName()
    {
       return userLast;
    }
    
+   /**
+   *  This getter is used to get the User's Age
+   */
    public int getAge()
    {
       return userAge;
    }
    
+   /**
+   *  This getter is used to get the User's Smoking Status
+   */
    public String getSmokingStatus()
    {
       return smokingStatus;
    }
    
+   /**
+   *  This getter is used to get the User's Height
+   */
    public double getHeight()
    {
       return userHeight;
    }
    
+   /**
+   *  This getter is used to get the User's Weight
+   */
    public double getWeight()
    {
       return userWeight;
@@ -138,6 +208,12 @@ public class Policy
    
    
    //calculate BMI
+   /**
+   *  This method is used to calculate the User's BMI
+   *  @param userWeight This is the stored weight in pounds
+   *  @param userHeight This is the stored height in inches
+   *  @return double BMI calculation
+   */
    public double getBMI()
    {
       final double factor = 703;
@@ -146,6 +222,11 @@ public class Policy
    }
    
    //calculate price
+   /**
+   *  This method is used to calculate the Policy's Price using the User's Calculated BMI
+   *  @param getBMI() This is the calculated BMI
+   *  @return double Price calculation
+   */
    public double getPrice()
    {
       double price = 600;
